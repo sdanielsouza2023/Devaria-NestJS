4f622464d0fe626384fe6306faeab583ee6c832a
sdanielsouza2023 <sdanielsouza35118@gmail.com>
2024-01-02 10:43:04 -0300

    configure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'log', 'warn']
+  });
+
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false
+    })
+  )
+  app.setGlobalPrefix('api')
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
